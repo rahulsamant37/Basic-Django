7cfd0e9adaa34e58aefb3568c0430ebbc94d501f
rahulsamant37 <rahulsamantcoc2@gmail.com>
2025-02-19 19:12:45 +0530

    learning template's in django

--- django_app/home/views.py
+++ django_app/home/views.py
@@ -5,7 +5,13 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, "index.html")
+    people = [
+        {'name': 'Rahul', 'age': 20},
+        {'name': 'Rohit', 'age': 21},
+        {'name': 'Raj', 'age': 22},
+        {'name': 'Ravi', 'age': 23}
+    ]
+    return render(request, 'index.html', {'people': people})
 
 def sucess(request):
     return HttpResponse("<h1>Success</h1>")
