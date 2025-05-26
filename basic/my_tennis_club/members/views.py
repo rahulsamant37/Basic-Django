8ef82bf9b33b952c275a189e2f90eed82de6faef
Rahul Samant <161972011+rahulsamant37@users.noreply.github.com>
2025-05-26 14:30:59 +0000

    feat: created a new view called main, that will deal with incoming requests to root of the project

--- basic/my_tennis_club/members/views.py
+++ basic/my_tennis_club/members/views.py
@@ -1,15 +1,15 @@
-from django.template import loader
 from django.http import HttpResponse
+from django.template import loader
 from .models import Member
 
 def members(request):
-    mymembers = Member.objects.all().values()
-    template = loader.get_template('all_members.html')
-    context = {
-      'mymembers': mymembers,
-    }
-    return HttpResponse(template.render(context, request))
-
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+  
 def details(request, id):
   mymember = Member.objects.get(id=id)
   template = loader.get_template('details.html')
@@ -17,3 +17,7 @@ def details(request, id):
     'mymember': mymember,
   }
   return HttpResponse(template.render(context, request))
+  
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
